215b08cec872d5d261f932904dc8b3cce39c9779
mansourehrousta <mansoureh.rousta@gmail.com>
2016-01-18 09:54:16 +0200

    Customer Feedback
    written in jQuery Mobile
    Customer feedback and subcontarctors management for insurance system

---
+++ js/app.js
@@ -0,0 +1,86 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.main', {
+    url: '/main1',
+    views: {
+      'menuContent': {
+        templateUrl: 'zerover1/main1.html'
+      }
+    }
+  })
+
+  .state('app.feedback', {
+      url: '/feedback',
+      views: {
+        'menuContent': {
+          templateUrl: 'zerover1/feedback.html'
+        }
+      }
+    })
+    .state('app.subcontractor', {
+      url: '/subcontractor',
+      views: {
+        'menuContent': {
+          templateUrl: 'zerover1/subcontractor.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+    
+    
+     .state('app.scoreband', {
+      url: '/scoreband',
+      views: {
+        'menuContent': {
+          templateUrl: 'zerover1/scoreband.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+    
+    
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
